61e2db1055a9f93178b94601e3b82fb58d1d022f
Manvi Tyagi <manvityagi770@gmail.com>
2020-03-27 17:12:04 +0530

    parsed

--- src/components/Problem.js
+++ src/components/Problem.js
@@ -1,8 +1,71 @@
 import React from "react";
+import axios from "axios";
+import IDE from "./IDE";
 
 class Problem extends React.Component {
+  state = {
+    pname: "",
+    pcode: "",
+    pstatement: "",
+    sizelmt: "",
+    timelmt: "",
+    author: "",
+    successfulSubmissions: 0,
+    totalSubmissions: 0,
+    languages: []
+  };
+
+  contestCode = this.props.match.params.contest_code;
+  problemCode = this.props.match.params.problem_code;
+
+  componentDidMount() {
+    axios({
+      method: "get",
+      url: `https://api.codechef.com/contests/${this.contestCode}/problems/${this.problemCode}`,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer f1a0593c6e762855d5ea68f5ebf8c9fa7bd4d80b`
+      }
+    })
+      .then(res => {
+        res = res.data.result.data.content;
+        //   console.log(res);
+        this.setState({
+          pname: res["problemName"],
+          pcode: res["problemCode"],
+          pstatement: res["body"],
+          sizelmt: res["sourceSizeLimit"],
+          timelmt: res["maxTimeLimit"],
+          author: res["author"],
+          successfulSubmissions: res["successfulSubmissions"],
+          totalSubmissions: res["totalSubmissions"],
+          lag: res["languagesSupported"]
+        });
+      })
+      .catch(err => {
+        console.log("NOT DONE");
+        console.log(err);
+      });
+  }
+
+  handleSubmit = e => {
+      this.props.history.push(``)
+  }
+
   render() {
-    return <h2>Problem PAGE</h2>;
+    return (
+      <>
+        <h1>{this.state.pname}</h1>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: this.state.pstatement
+          }}
+        ></div>
+        <h1>successful Submissions - {this.state.successfulSubmissions}</h1>
+        <IDE></IDE>
+        <button onClick={this.handleSubmit}>Submit</button>
+      </>
+    );
   }
 }
 
