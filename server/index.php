9d238b4706391856aabf38309fcc6e8e2dd3e276
yashpreetbathla <32976561+yashpreetbathla@users.noreply.github.com>
2020-03-30 22:48:41 +0530

    Update index.php

--- server/index.php
+++ server/index.php
@@ -71,8 +71,8 @@ function make_curl_request($url, $post = FALSE, $headers = array())
 
 function main(){
 
-    $config = array('client_id'=> '403e3bf719e72263e079c08710ec6820',
-        'client_secret' => 'eb2ded455dcc329c52740562f2ff603c',
+    $config = array('client_id'=> '7f066223fed7e3268fb739aa17e5d026',
+        'client_secret' => '1e054c722b0bc5d8d3f7e34cdaf1f894',
         'api_endpoint'=> 'https://api.codechef.com/',
         'authorization_code_endpoint'=> 'https://api.codechef.com/oauth/authorize',
         'access_token_endpoint'=> 'https://api.codechef.com/oauth/token',
